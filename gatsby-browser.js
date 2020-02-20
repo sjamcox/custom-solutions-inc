ed1b8dfb2207ad4bba72e86c12e4802fbd16ac94
Sam Cox <sam@coxdesign.co>
2020-02-20 05:51:00 -0800

    Removed font import to fix FOUC

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,8 +2,6 @@ import React from 'react'
 import "./src/styles/global.css"
 import { ContextProvider } from "./src/context/Context"
 import { GlobalStyle } from './src/utils/GlobalStyle'
-import 'typeface-roboto'
-import 'typeface-gelasio'
 
 export const wrapRootElement = ({ element }) => (
     <ContextProvider>
